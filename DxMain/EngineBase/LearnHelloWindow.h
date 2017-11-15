969d19b0157054683617dd1c2d3210885024b958
JinzerY <Jinzer_Yang@163.com>
2017-11-15 16:13:45 +0800

    add triangle

--- DxMain/EngineBase/LearnHelloWindow.h
+++ DxMain/EngineBase/LearnHelloWindow.h
@@ -2,6 +2,7 @@
 
 #include "DXSample.h"
 
+using namespace DirectX;
 using namespace Microsoft::WRL;
 
 class LearnHelloWindow : public DXSample
@@ -19,16 +20,30 @@ private:
 
 	static const UINT FrameCount = 2;
 
+	struct Vertex
+	{
+		XMFLOAT3 position;
+		XMFLOAT4 color;
+	};
+
 	//pipeline objects
+	CD3DX12_VIEWPORT m_viewport;
+	CD3DX12_RECT m_scissorRect;
+
 	ComPtr<IDXGISwapChain3> m_swapChain;
 	ComPtr<ID3D12Device> m_device;
 	ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
 	ComPtr<ID3D12CommandAllocator> m_commandAllocator;
 	ComPtr<ID3D12CommandQueue> m_commandQueue;
+	ComPtr<ID3D12RootSignature> m_rootSignature;
 	ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
 	ComPtr<ID3D12PipelineState> m_pipelineState;
 	ComPtr<ID3D12GraphicsCommandList> m_commandList;
 	UINT m_rtvDescriptorSize;
+
+	//app resource
+	ComPtr<ID3D12Resource> m_vertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
 
 	//synchronization objects
 	UINT m_frameIndex;
